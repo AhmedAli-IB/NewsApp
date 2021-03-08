cd024a0ce0c0e0681e5bbab9abaa466e5b9f0b9b
Ahmed Ali <ahmed.amen@ibtikar.net.sa>
2021-03-08 13:52:49 +0200

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ahmed Ali on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
